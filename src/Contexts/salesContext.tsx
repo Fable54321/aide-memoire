3775fa23179cbb783200373f2471942090e84e6c
fable54321 <timothe5@hotmail.com>
2026-05-28 12:01:51 -0400

    first commit

--- src/Contexts/salesContext.tsx
+++ src/Contexts/salesContext.tsx
@@ -1,5 +1,5 @@
 
-import { createContext, useContext, useState, useEffect ,type ReactNode } from "react"
+import { createContext, useContext, useState, useEffect ,type ReactNode, useCallback } from "react"
 
 
 export type Client = {
@@ -8,9 +8,23 @@ export type Client = {
 }
 
 
+type Quotation = {
+    id: number,
+    client_id: number,
+    vegetable_id: number,
+    price: number,
+    quotation_date: string,
+}
+
 type SalesContextType = {
     clients: Client[],
-    setClients: React.Dispatch<React.SetStateAction<Client[]>>
+    setClients: React.Dispatch<React.SetStateAction<Client[]>>,
+    postQuotation: (
+        clientId: number,
+        vegetableId: number,
+        price: number,
+        date: string,
+    ) => Promise<Quotation>
 }
 
 
@@ -25,7 +39,7 @@ export const SalesProvider = ({children}: SalesProviderProps) => {
     const [clients, setClients] = useState<Client[]>([])
 
     useEffect(() => {
-        fetch("https:/api.vegibec-portail.com/unprotected/clients")
+        fetch("https://api.vegibec-portail.com/unprotected/clients")
         .then(res => res.json())
         .then(data => setClients(data))
     }, [])
@@ -35,8 +49,23 @@ export const SalesProvider = ({children}: SalesProviderProps) => {
         console.log(clients)
     },[clients])
 
+    const postQuotation = useCallback((clientId: number, vegetableId: number, price: number, date: string) => {
+        return fetch("https://api.vegibec-portail.com/unprotected/quotations", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                client_id: clientId,
+                vegetable_id: vegetableId,
+                price: price,
+                quotation_date: date
+            })
+        }).then(res => res.json())
+    },[])
+
     return (
-        <SalesContext.Provider value={{clients, setClients}}>
+        <SalesContext.Provider value={{clients, setClients, postQuotation}}>
             {children}
         </SalesContext.Provider>
     )
